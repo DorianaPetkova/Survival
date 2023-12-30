9f774ee6776a11b4c170beb7858b32edaea5c54e
Dorian <doriana.petkova@gmail.com>
2023-12-31 00:49:35 +0200

    Character animation + movement

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] private int speed = 50;
+    private Vector2 move;
+    [SerializeField] private Animator animator;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    private void OnMovement(InputValue value)
+    {
+        move = value.Get<Vector2>();
+        if (move.x != 0 || move.y != 0)
+        {
+            animator.SetFloat("X", move.x);
+            animator.SetFloat("Y", move.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else
+            animator.SetBool("IsWalking", false);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+}
