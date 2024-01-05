2eba209c34f780ac894c130807ded3b759a9d098
Dorian <doriana.petkova@gmail.com>
2024-01-06 00:44:38 +0200

    Added physics

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -5,19 +5,15 @@ using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] private int speed = 120;
+    [SerializeField] private int speed = 50;
     private Vector2 move;
     [SerializeField] private Animator animator;
     private Rigidbody2D rb;
-
-    [SerializeField] private float maxSkyHeight = 106;  // Set your maximum sky height here
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
-
     private void OnMovement(InputValue value)
     {
         move = value.Get<Vector2>();
@@ -25,26 +21,16 @@ public class movement : MonoBehaviour
         {
             animator.SetFloat("X", move.x);
             animator.SetFloat("Y", move.y);
+
             animator.SetBool("IsWalking", true);
         }
         else
-        {
             animator.SetBool("IsWalking", false);
-        }
     }
-
     private void FixedUpdate()
     {
-        // Move the character
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
-
-        // Check sky boundary
-        if (transform.position.y > maxSkyHeight)
-        {
-            // Clamp the character's position to stay within the sky boundary
-            Vector3 newPosition = transform.position;
-            newPosition.y = maxSkyHeight;
-            transform.position = newPosition;
-        }
     }
+
+
 }
